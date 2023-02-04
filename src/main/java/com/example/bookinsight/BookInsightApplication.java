d3a42fb2b5f28f0dfb45a8814b02caf6cf0ec800
Ibrohimjon <ibrohimjonyursunov50@gmail.com>
2023-02-04 14:37:12 +0500

    Start project

---
+++ src/main/java/com/example/bookinsight/BookInsightApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookInsightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookInsightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookinsight/BookInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookInsightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
